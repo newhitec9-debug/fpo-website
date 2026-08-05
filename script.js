7d2ba3f0936bb25519ef65baeb048f40751bcf98
newhitec9-debug <newhitec9@gmail.com>
2026-08-05 09:47:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 alert("🎉 Welcome to Bundelkhand Organic Farmer Producer Company Limited");
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.querySelector("form");
+
+    if(form){
+
+        form.addEventListener("submit", function(e){
+
+            e.preventDefault();
+
+            let mobile = document.getElementById("mobile").value;
+            let aadhaar = document.getElementById("aadhaar").value;
+            let name = document.getElementById("name").value;
+
+            if(name==""){
+                alert("Please Enter Farmer Name");
+                return;
+            }
+
+            if(mobile.length!=10){
+                alert("Enter Valid Mobile Number");
+                return;
+            }
+
+            if(aadhaar.length!=12){
+                alert("Enter Valid Aadhaar Number");
+                return;
+            }
+
+            alert("🎉 Farmer Registration Successful!");
+        });
+
+    }
+
+});
